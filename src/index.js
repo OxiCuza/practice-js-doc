559743d69715b0bce18c4e16705601dd9d246a58
Oxi_Cuza <oxicusa@gmail.com>
2021-09-01 16:07:54 +0700

    create some property

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// @ts-check
+
+/**
+ * Registered student name
+ * @constant {string}
+ */
+const STUDENT_NAME = 'Jhon Fool';
+
+/**
+ * Array of grades
+ * @constant {Array<number>}
+ */
+const GRADES = [98, 70, 80];
+
+/**
+ * Example create TODO object
+ * @constant {object}
+ * @property {number} id - an ID.
+ * @property {string} text - some text if you want.
+ */
+const TODO = {
+    id: 1,
+    text: 'Hello World !',
+};
